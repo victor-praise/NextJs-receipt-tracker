a86158538d467d27f4c897c52a09984759de2eea
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-08-17 15:35:44 -0400

    fix: Correct typo in ManagePlan component description

--- NextJs-receipt-tracker/app/manage-plan/page.tsx
+++ NextJs-receipt-tracker/app/manage-plan/page.tsx
@@ -5,7 +5,7 @@ function ManagePlan() {
   return (
     <div className="container xl:max-w-5xl mx-auto p-4 md:p-0">
        <h1 className="text-2xl font-bold mb-4 my-8">Manage Your Plan</h1>
-       <p className="text-gray-600 mb-8">Manage your subscription and billing details here.</p>
+       <p className="text-gray-600 mb-8">Manage your subscription and billing details here!!!!!.</p>
         <SchematicComponent componentId={process.env.NEXT_PUBLIC_SCHEMATIC_CUSTOMER_PORTAL_COMPONENT_ID} />
     </div>
   )
