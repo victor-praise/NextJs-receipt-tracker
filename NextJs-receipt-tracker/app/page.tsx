b4e1461fce918de3f68d33cfb88727e7a40bbd3f
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-08-18 21:14:50 -0400

    built home page

--- NextJs-receipt-tracker/app/page.tsx
+++ NextJs-receipt-tracker/app/page.tsx
@@ -4,7 +4,18 @@
 export default function Home() {
   return (
     <>
-     <div>Hello world</div>
+     <div>
+      <section>
+      <div className="container mx-auto px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">Intelligent Receipt Scanning</h1>
+            <p className="mx-auto max-w-[700] text-gray-500 md:text-xl dark:text-gray-400">Scan, analyze, and organize your receipts with AI-powered precision. Save time and gain insights from your expenses</p>
+          </div>
+        </div>
+      </div>
+      </section>
+     </div>
     </>
   );
 }
