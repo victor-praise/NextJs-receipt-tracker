67efe4c1b38fb6451caf73d4c617414d6ce658a6
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-08-22 22:46:39 -0400

    fix

--- NextJs-receipt-tracker/app/page.tsx
+++ NextJs-receipt-tracker/app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
                 </li>
                 <li className='flex items-center'>
                   <Check className="text-green-500 mr-2 h-5 w-5" />
-                  <span> 7-day history</span>
+                  <span> 7-day history!!!!</span>
                 </li>
               </ul>
               <div></div>
