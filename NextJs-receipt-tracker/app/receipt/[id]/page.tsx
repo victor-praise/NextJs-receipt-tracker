81ef8bc845f23c1c8500166657c1333efc6ad89e
victor-praise <61267951+victor-praise@users.noreply.github.com>
2025-09-08 18:52:50 -0400

    setting up view receipt page

--- NextJs-receipt-tracker/app/receipt/[id]/page.tsx
+++ NextJs-receipt-tracker/app/receipt/[id]/page.tsx
@@ -1,9 +1,27 @@
 'use client'
-import { useParams } from 'next/navigation';
-import React from 'react'
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { useQuery } from 'convex/react';
+import { Router } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 function Receipt() {
     const params = useParams<{id:string}>();
+  const router = useRouter();
+    const {receiptId, setReceiptId} = useState<Id<"receipts"> | null>(null);
+
+    const receipt = useQuery(api.receipts.getReceiptById, receiptId ? {id:receiptId}: "skip");
+
+    useEffect(()=>{
+      try {
+        const id = params.id as Id<"receipts">;
+        setReceiptId(id);
+      } catch (error) {
+        console.error("Invalid receipt ID: ", error);
+        router.push("/");
+      }
+    },[params.id, router])
   return (
     <div>Receipt</div>
   )
